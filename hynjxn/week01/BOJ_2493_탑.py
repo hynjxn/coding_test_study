385b4c1b517154e4845c67d1ece1b1371605d5bf
hyunjin <princess7x7@naver.com>
2023-07-04 09:55:31 +0900

    ADD: [BOJ] 탑 / 골드5 / 풀이 참고하여 해결

--- hynjxn/week01/BOJ_2493_탑.py
+++ hynjxn/week01/BOJ_2493_탑.py
@@ -1,17 +1,43 @@
+# 백준 1874 스택 수열
+# https://www.acmicpc.net/problem/2493
+# 시간 초과로 https://jjangsungwon.tistory.com/44 참고 후 해결
+
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 tow_h = list(map(int, input().split()))
-result = [0] # 어차피 맨 왼쪽 탑의 신호는 수신 불가능
-while len(tow_h) > 1:
-    i = 1
-    while True:
-        if i > N-1 :
-            result.append(0)
-            break
-        if tow_h[-1] <= tow_h[-1-i]:
-            result.append(N-i)
-            break
-        i += 1
+result = []
 
+# 시간 초과 코드 -> 맨 오른쪽 탑부터 왼쪽의 탑을 완전 탐색
+# while len(tow_h) > 1:
+#     i = 1
+#     towers = len(tow_h)
+#     while True:
+#         if i > towers-1:
+#             result.append(0)
+#             break
+#         if tow_h[-1] <= tow_h[-1-i]:
+#             result.append(towers-i)
+#             break
+#         i += 1
+#     tow_h.pop()
+# print([0]+result[::-1])
+
+# 왼쪽부터 탐색하면서 이전까지의 탑의 정보를 스택에 저장
+# 스택에 현재 인덱스의 탑보다 작은 탑이 있다면 pop시킴 -> 오른쪽의 탑이 탐색할 필요가 없기 때문
+stk = []
+
+for i in range(N):
+    while stk:
+        if stk[-1][1] > tow_h[i]:
+            result.append(stk[-1][0])
+            break
+        else:
+            stk.pop()
+    if not stk:
+        # 수신할 수 있는 탑이 없는 경우
+        result.append(0)
+
+    stk.append([i+1, tow_h[i]])
+print(*result)
